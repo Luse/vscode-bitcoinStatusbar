0288026936f163484c9bf297d8b1b3040687bec7
johan damm <johan.b.damm@gmail.com>
2017-05-04 23:19:03 +0200

    Correctly display updated text from server

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,14 @@
 import * as vscode from 'vscode';
 import * as https from 'https';
 
-
+let bitcoinItem
 export function activate(context: vscode.ExtensionContext) {
+    bitcoinItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+    bitcoinItem.text = "loading";
+    bitcoinItem.show();
+    
     refresh()
-
+    setInterval(refresh, 60000);
 }
 
 
@@ -19,7 +23,6 @@ function refresh(): void {
 
 function createItem(): void {
     const url = "https://xbtprovider.com/api/rates?currency=eur";
-    console.log('starting');
     httpGet(url).then(response => {
         const responseObj = JSON.parse(response)
         updateStatusWithResult(responseObj.data);
@@ -28,9 +31,7 @@ function createItem(): void {
 }
 function updateStatusWithResult(result): void {
     var data = result.usdAverage
-    const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-    item.text = data;
-    item.show();
+    bitcoinItem.text = data.toString();
 }
 
 function httpGet(url): Promise<string> {
